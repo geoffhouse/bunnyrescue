05b9d5a11a22ef71aceef53b391ce393c15294b1
Geoff House <geoffhouse@users.noreply.github.com>
2022-03-28 21:44:08 +0100

    removed unused code

--- src/routes/server.js
+++ src/routes/server.js
@@ -3,7 +3,6 @@
 const express = require("express");
 const router = express.Router();
 const serverGetDetails = require("@components/server-getdetails");
-const serverCheckCode = require("@components/server-checkcode");
 const authUser = require("@middleware/auth-user");
 const hashResponse = require("@services/hash-response");
 const asyncHandler = require("express-async-handler");
@@ -13,17 +12,6 @@ router.get(
     authUser,
     asyncHandler(async (req, res) => {
         const result = await serverGetDetails(req);
-        hashResponse(res, req, {
-            status: result ? "success" : "failure",
-            data: result,
-        });
-    })
-);
-
-router.get(
-    "/checkcode/:code",
-    asyncHandler(async (req, res) => {
-        const result = await serverCheckCode(req);
         hashResponse(res, req, {
             status: result ? "success" : "failure",
             data: result,
