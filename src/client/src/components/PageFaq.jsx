4a95702ce20659796c0874d3a93a4a23a5ee9dc4
Geoff House <geoffhouse@users.noreply.github.com>
2022-04-08 21:17:38 +0100

    add ko-fi donate button

--- src/client/src/components/PageFaq.jsx
+++ src/client/src/components/PageFaq.jsx
@@ -3,6 +3,19 @@ import Navigation from "./Navigation";
 import BackBar from "./BackBar";
 import ContentWrapper from "./ContentWrapper";
 import TemplatedContent from "./TemplatedContent";
+import { styled } from "@mui/material/styles";
+import KofiButton from "kofi-button";
+import Box from "@mui/material/Box";
+
+const Title = styled("h3")(() => ({
+    marginBottom: "0.4rem",
+    marginTop: "1rem",
+}));
+
+const Text = styled("div")(() => ({
+    marginBottom: "0.4rem",
+    marginTop: "0.4rem",
+}));
 
 export default function PageFaq() {
     return (
@@ -15,6 +28,16 @@ export default function PageFaq() {
                 scrollable
             >
                 <TemplatedContent templateName="faq" />
+
+                <div>
+                    <Title>Support</Title>
+                    <Text>
+                        If you'd like to buy me a coffee and support more projects like this, please click here:
+                        <Box sx={{ marginTop: "1rem" }}>
+                            <KofiButton color="#0a9396" title="Donate" kofiID="F1F8C0UK8" />
+                        </Box>
+                    </Text>
+                </div>
             </ContentWrapper>
             <Navigation></Navigation>
         </>
